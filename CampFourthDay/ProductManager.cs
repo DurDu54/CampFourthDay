bb63fa9bc4b8b06f90c4af5466b118230bc0e605
Mucahit DURDU <Mucahit DURDU@DURDU>
2022-06-29 20:37:04 +0300

    Proje dosyası ekle.

---
+++ CampFourthDay/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampFourthDay_OOP_
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            //referans tip Burada yaptığımız değişiklikler heapde olduğu için program.cs de de değişir
+            //değer tip olsaydı sadece sadece bu metot içinde değişiklik olacaktı program.cs de bişey değişmeyecekti
+
+            Console.WriteLine(product.ProductName + " eklendi.");
+
+        }
+        public void update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+
+        }
+    }
+}
---
+++ Colections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Colections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Engin", "Mücahit", "Hakan" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //isimler[3] = "Celil";
+            //Console.WriteLine(isimler[3]);
+                //***burada program çalışmaz çünkü bizim dizimiz 3 elemanlı olaarka oluşturuldu ve eleman sayısı sonradan depiştirilemez
+            
+            
+            List<string> isimler2 = new List<string>() { "Mücahit", "Hakan"};
+            
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            isimler2.Add("Engin");
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("Celil");
+            Console.WriteLine(isimler2[3]);
+                //***liste oluşturulurken boyut istenmez bu sayede sonradan eleman ekleme işleminde sorun çıkmaz
+                //istediğiimiz elemanı istediğimiz yerde istediğimiz kadar ekleyebiliriz 
+
+        }
+    }
+}
